70a94e710bdf23c08eb141be271152b0caa6dbfd
Fabricio Dias <fabricio270909@gmail.com>
2025-01-19 10:40:33 -0300

    Account class structure is complete

---
+++ BankAccountManager/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿using BankAccountManager.Entities.Exceptions;
+
+namespace BankAccountManager.Entities;
+
+public class Account
+{
+    public int Number { get; set; }
+    public string Holder { get; set; }
+    public double Balance { get; set; }
+    public double WithDrawLimit { get; set; }
+
+    public Account()
+    {
+    }
+
+    public Account(int number, string holder, double balance, double withDrawLimit)
+    {
+        Number = number;
+        Holder = holder;
+        Balance = balance;
+        WithDrawLimit = withDrawLimit;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    public void WithDraw(double amount)
+    {
+        if (Balance <= 0.00)
+        {
+            throw new DomainException("Withdraw error: No enough balance!");
+        }
+
+        if (amount > WithDrawLimit)
+        {
+            throw new DomainException("Withdraw error: you have no limit!");
+        }
+
+        Balance -= amount;
+    }
+}
