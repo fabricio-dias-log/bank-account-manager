7a4b99d4db8cd98acbe5233136e82d89da960423
Fabricio Dias <fabricio270909@gmail.com>
2025-01-19 11:09:13 -0300

    Program workflow is complete

--- BankAccountManager/Program.cs
+++ BankAccountManager/Program.cs
@@ -1,9 +1,47 @@
-﻿namespace BankAccountManager;
+﻿using System.Globalization;
+using BankAccountManager.Entities;
+using BankAccountManager.Entities.Exceptions;
+
+namespace BankAccountManager;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter account data:");
+
+        try
+        {
+            Console.Write("Number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Holder: ");
+            string accountHolder = Console.ReadLine();
+
+            Console.Write("Initial balance: ");
+            double accountBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(accountNumber, accountHolder, accountBalance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double withdrawAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            account.WithDraw(withdrawAmount);
+
+            Console.WriteLine($"New balance: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+        catch (DomainException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine($"Format error: {e.Message}");
+        }
     }
 }
